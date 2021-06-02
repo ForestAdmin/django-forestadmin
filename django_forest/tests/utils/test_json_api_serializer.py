7a4fa253d1686c1c05c0c13cdae678264a3cf69b
GuillaumeCisco <guillaumecisco@gmail.com>
2021-06-02 16:32:35 +0200

    test: add some tests on json api serializer

--- django_forest/tests/utils/test_json_api_serializer.py
+++ django_forest/tests/utils/test_json_api_serializer.py
@@ -3,7 +3,7 @@ import copy
 from django.test import TestCase
 
 from django_forest.tests.fixtures.schema import test_schema
-from django_forest.tests.models import Article, Session, Question
+from django_forest.tests.models import Article, Session, Question, Choice
 
 from django_forest.utils.collection import Collection
 from django_forest.utils.json_api_serializer import JsonApiSchema
@@ -11,7 +11,7 @@ from django_forest.utils.schema import Schema
 
 
 class UtilsJsonApiSerializerTests(TestCase):
-    fixtures = ['article.json', 'publication.json', 'session.json', 'question.json']
+    fixtures = ['article.json', 'publication.json', 'session.json', 'question.json', 'choice.json']
 
     def setUp(self):
         Schema.schema = copy.deepcopy(test_schema)
@@ -29,7 +29,31 @@ class UtilsJsonApiSerializerTests(TestCase):
         Schema.handle_json_api_serializer()
         schema = JsonApiSchema._registry['QuestionSchema']
         question = Question.objects.get(pk=1)
-        data = schema().dump([question], many=True)
+        data = schema().dump(question)
+        self.assertEqual(data, {
+            'data': {
+                'type': 'question',
+                'relationships': {
+                    'choice': {
+                        'links': {
+                            'related': '/forest/Question/1/relationships/Choice'
+                        }
+                    }
+                },
+                'attributes': {
+                    'question_text': 'what is your favorite color?',
+                    'pub_date': '2021-06-02T13:52:53.528000+00:00',
+                    'id': 1.0
+                },
+                'id': 1.0
+            }
+        })
+
+    def test_json_api_serializer_many(self):
+        Schema.handle_json_api_serializer()
+        schema = JsonApiSchema._registry['QuestionSchema']
+        queryset = Question.objects.all()
+        data = schema().dump(queryset, many=True)
         self.assertEqual(data, {
             'data': [
                 {
@@ -47,6 +71,68 @@ class UtilsJsonApiSerializerTests(TestCase):
                         'id': 1.0
                     },
                     'id': 1.0
+                },
+                {
+                    'type': 'question',
+                    'relationships': {
+                        'choice': {
+                            'links': {
+                                'related': '/forest/Question/2/relationships/Choice'
+                            }
+                        }
+                    },
+                    'attributes': {
+                        'question_text': 'do you like chocolate?',
+                        'pub_date': '2021-06-02T15:52:53.528000+00:00',
+                        'id': 2.0
+                    },
+                    'id': 2.0
+                }
+            ]
+        })
+
+    def test_json_api_serializer_foreign_key(self):
+        Schema.handle_json_api_serializer()
+        schema = JsonApiSchema._registry['ChoiceSchema']
+        choice = Choice.objects.get(pk=1)
+        data = schema(include_data=['question']).dump(choice)
+        self.assertEqual(data, {
+            'data': {
+                'type': 'choice',
+                'attributes': {
+                    'votes': 0.0,
+                    'choice_text': 'yes',
+                    'id': 1.0
+                },
+                'id': 1.0,
+                'relationships': {
+                    'question': {
+                        'links': {
+                            'related': '/forest/Choice/1/relationships/Question'
+                        },
+                        'data': {
+                            'type': 'question',
+                            'id': '1'
+                        }
+                    }
+                }
+            },
+            'included': [
+                {
+                    'type': 'question',
+                    'attributes': {
+                        'pub_date': '2021-06-02T13:52:53.528000+00:00',
+                        'question_text': 'what is your favorite color?',
+                        'id': 1.0
+                    },
+                    'id': 1.0,
+                    'relationships': {
+                        'choice': {
+                            'links': {
+                                'related': '/forest/Question/1/relationships/Choice'
+                            }
+                        }
+                    }
                 }
             ]
         })
@@ -55,50 +141,46 @@ class UtilsJsonApiSerializerTests(TestCase):
         Schema.handle_json_api_serializer()
         schema = JsonApiSchema._registry['SessionSchema']
         session = Session.objects.get(pk='foobar1234')
-        data = schema().dump([session], many=True)
+        data = schema().dump(session)
         self.assertEqual(data, {
-            'data': [
-                {
-                    'type': 'session',
-                    'attributes': {
-                        'session_key': 'foobar1234',
-                        'expire_date': '2021-06-02T13:48:36.039000+00:00',
-                        'session_data': 'foo',
-                        'id': 'foobar1234'
-                    },
+            'data': {
+                'type': 'session',
+                'attributes': {
+                    'session_key': 'foobar1234',
+                    'expire_date': '2021-06-02T13:48:36.039000+00:00',
+                    'session_data': 'foo',
                     'id': 'foobar1234'
-                }
-            ]
+                },
+                'id': 'foobar1234'
+            }
         })
 
     def test_json_api_serializer_many_to_many(self):
         Schema.handle_json_api_serializer()
         schema = JsonApiSchema._registry['ArticleSchema']
         article = Article.objects.get(pk=1)
-        data = schema(include_data=['publications']).dump([article], many=True)
+        data = schema(include_data=['publications']).dump(article)
         self.assertEqual(data, {
-            'data': [
-                {
-                    'type': 'article', 'id': 1.0,
-                    'attributes': {
-                        'headline': 'Django lets you build Web apps easily',
-                        'id': 1.0
-                    },
-                    'relationships': {
-                        'publications': {
-                            'links': {
-                                'related': '/forest/Article/1/relationships/Publication'
-                            },
-                            'data': [
-                                {
-                                    'type': 'publication',
-                                    'id': '1'
-                                }
-                            ]
-                        }
+            'data': {
+                'type': 'article', 'id': 1.0,
+                'attributes': {
+                    'headline': 'Django lets you build Web apps easily',
+                    'id': 1.0
+                },
+                'relationships': {
+                    'publications': {
+                        'links': {
+                            'related': '/forest/Article/1/relationships/Publication'
+                        },
+                        'data': [
+                            {
+                                'type': 'publication',
+                                'id': '1'
+                            }
+                        ]
                     }
                 }
-            ],
+            },
             'included': [
                 {
                     'type': 'publication',
