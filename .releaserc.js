1d30fdb05be82d3f8fd2b2bbd5af820e0e780bf3
GuillaumeCisco <guillaumecisco@gmail.com>
2021-06-09 12:21:31 +0200

    chore: rework .releaserc

--- .releaserc.js
+++ .releaserc.js
@@ -69,4 +69,5 @@ module.exports = {
       }
     ],
   ],
+  'branches': ['+([0-9])?(.{+([0-9]),x}).x', 'main', 'next', 'next-major', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}]
 }
