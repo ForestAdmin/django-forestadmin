bbc16f458d2ce0653d56b01eb93032e84912183e
GuillaumeCisco <guillaumecisco@gmail.com>
2021-06-10 14:20:31 +0200

    chore: add tests

--- django_forest/utils/cors.py
+++ django_forest/utils/cors.py
@@ -3,6 +3,10 @@ from django.conf import settings
 from django_forest.utils.get_forest_setting import get_forest_setting
 
 
+def get_list_setting(setting):
+    return [x for x in get_forest_setting(setting, '').split(',') if x != '']
+
+
 def set_cors():
     settings.INSTALLED_APPS += ['corsheaders']
     try:
@@ -11,14 +15,14 @@ def set_cors():
         common_middleware_index = 0
     settings.MIDDLEWARE.insert(common_middleware_index, 'corsheaders.middleware.CorsMiddleware')
 
-    default_cors_allowed_origins = ['http://localhost:4200']
-    cors_allowed_origins = get_forest_setting('CORS_ALLOWED_ORIGINS', '').split(',')
+    cors_allowed_origins = ['http://localhost:4200']
+    cors_allowed_origins.extend(get_list_setting('CORS_ALLOWED_ORIGINS'))
 
-    default_cors_allowed_origins_regexes = [r'/\.forestadmin\.com$/']
-    cors_allowed_origins_regexes = get_forest_setting('CORS_ALLOWED_ORIGIN_REGEXES', '').split(',')
+    cors_allowed_origins_regexes = [r'/\.forestadmin\.com$/']
+    cors_allowed_origins_regexes.extend(get_list_setting('CORS_ALLOWED_ORIGIN_REGEXES'))
 
-    settings.CORS_ALLOWED_ORIGINS = default_cors_allowed_origins.extend(cors_allowed_origins)
-    settings.CORS_ALLOWED_ORIGIN_REGEXES = default_cors_allowed_origins_regexes.extend(cors_allowed_origins_regexes)
+    settings.CORS_ALLOWED_ORIGINS = cors_allowed_origins
+    settings.CORS_ALLOWED_ORIGIN_REGEXES = cors_allowed_origins_regexes
     settings.CORS_URLS_REGEX = r'^/forest(/.*)?$'  # restrict it to /forest
     settings.CORS_PREFLIGHT_MAX_AGE = 86400  # one day
     settings.CORS_ALLOW_CREDENTIALS = True
