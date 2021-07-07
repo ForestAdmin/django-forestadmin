ee9bc1cbf6799f90ef14ea54a26605d8054f816d
GuillaumeCisco <guillaumecisco@gmail.com>
2021-07-07 19:27:08 +0200

    test: add more tests

--- django_forest/tests/middleware/test_permissions.py
+++ django_forest/tests/middleware/test_permissions.py
@@ -83,3 +83,39 @@ class MiddlewarePermissionsTests(TestCase):
             'page[size]': '15'
         })
         self.assertEqual(response.status_code, 403)
+
+
+class MiddlewarePermissionsCachedTests(TestCase):
+
+    def setUp(self):
+        set_middlewares()
+        Schema.schema = copy.deepcopy(test_schema)
+        Schema.handle_json_api_schema()
+        self.url = reverse('resources:list', kwargs={'resource': 'Question'})
+        self.client = self.client_class(HTTP_AUTHORIZATION='Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUiLCJlbWFpbCI6Imd1aWxsYXVtZWNAZm9yZXN0YWRtaW4uY29tIiwiZmlyc3RfbmFtZSI6Ikd1aWxsYXVtZSIsImxhc3RfbmFtZSI6IkNpc2NvIiwidGVhbSI6Ik9wZXJhdGlvbnMiLCJyZW5kZXJpbmdfaWQiOjEsImV4cCI6MTYyNTY3OTYyNi44ODYwMTh9.mHjA05yvMr99gFMuFv0SnPDCeOd2ZyMSN868V7lsjnw')
+
+    def tearDown(self):
+        # reset _registry after each test
+        JsonApiSchema._registry = {}
+
+    @mock.patch('middleware.PermissionMiddleware.get_app_name', return_value='django_forest:resources')
+    @mock.patch('jose.jwt.decode', return_value={'id': 1, 'rendering_id': 1})
+    @mock.patch('requests.get', return_value=mocked_requests(mocked_config, 200))
+    def test_list(self, mocked_requests, mocked_decode, mocked_app_name):
+        response = self.client.get(self.url, {
+            'page[number]': '1',
+            'page[size]': '15'
+        })
+        self.assertEqual(response.status_code, 200)
+
+    @mock.patch('middleware.PermissionMiddleware.get_app_name', return_value='django_forest:resources')
+    @mock.patch('jose.jwt.decode', return_value={'id': 1, 'rendering_id': 1})
+    @mock.patch('requests.get', return_value=mocked_requests(mocked_config, 200))
+    @mock.patch('django_forest.utils.permissions.Permission.fetch_permissions')
+    def test_list_once_again(self, mocked_fetch_permissions, mocked_requests, mocked_decode, mocked_app_name):
+        response = self.client.get(self.url, {
+            'page[number]': '1',
+            'page[size]': '15'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(mocked_fetch_permissions.called)
