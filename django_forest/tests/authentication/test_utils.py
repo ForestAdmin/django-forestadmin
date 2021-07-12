92eb624f3e70cba33658a475d3c4ab61b01cbe6d
GuillaumeCisco <guillaumecisco@gmail.com>
2021-07-12 21:25:06 +0200

    test: fix tests

--- django_forest/tests/authentication/test_utils.py
+++ django_forest/tests/authentication/test_utils.py
@@ -1,16 +1,7 @@
-from unittest import mock
-
 from django.test import TestCase, override_settings
 
 
 class AuthenticationUtilsTests(TestCase):
-
-    def setUp(self):
-        self.patcher = mock.patch('django.urls.reverse')
-
-    def tearDown(self):
-        self.patcher.stop()
-
     def test_get_callback_url(self):
         from django_forest.authentication.utils import get_callback_url
         callback_url = get_callback_url()
