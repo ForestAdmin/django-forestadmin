6886e3b3b7931d94a29a2585ed56be658fb1d838
GuillaumeCisco <guillaumecisco@gmail.com>
2021-07-13 16:06:40 +0200

    chore: chart support

--- django_forest/urls.py
+++ django_forest/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 
 from . import views
 from .authentication import urls as authentication_urls
+from .stats import urls as stats_urls
 from .resources import urls as collection_urls
 
 app_name = 'django_forest'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('/authentication', include(authentication_urls)),
+    path('/stats', include(stats_urls)),
     path('/<slug:resource>', include(collection_urls)),
 ]
