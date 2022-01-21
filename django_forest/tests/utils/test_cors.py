60b833e1219226ba89b603abb992dbbf4bcf571b
Valentin Monté <valentin.monte@yahoo.fr>
2022-01-21 09:56:44 +0100

    fix(security): remove localhost from cors_allowed_origins it's not a good idea to use localhost as allowed origins and it's not usefull in our case

--- django_forest/tests/utils/test_cors.py
+++ django_forest/tests/utils/test_cors.py
@@ -11,7 +11,6 @@ class UtilsCorsTests(TestCase):
         self.assertTrue('corsheaders.middleware.CorsMiddleware' in settings.MIDDLEWARE)
         common_middleware_index = settings.MIDDLEWARE.index('django.middleware.common.CommonMiddleware')
         self.assertEqual(settings.MIDDLEWARE.index('corsheaders.middleware.CorsMiddleware'), common_middleware_index - 1)
-        self.assertEqual(settings.CORS_ALLOWED_ORIGINS, ['http://localhost:4200'])
         self.assertEqual(settings.CORS_ALLOWED_ORIGIN_REGEXES, ['.*\\.forestadmin\\.com.*'])
         self.assertEqual(settings.CORS_URLS_REGEX, r'^/forest(/.*)?$')
         self.assertEqual(settings.CORS_PREFLIGHT_MAX_AGE, 86400)
@@ -23,8 +22,7 @@ class UtilsCorsTests(TestCase):
         self.assertTrue('corsheaders.middleware.CorsMiddleware' in settings.MIDDLEWARE)
         self.assertEqual(settings.MIDDLEWARE, ['corsheaders.middleware.CorsMiddleware'])
 
-    @override_settings(FOREST={'CORS_ALLOWED_ORIGINS': 'http://foo.com,https://bar.com', 'CORS_ALLOWED_ORIGIN_REGEXES': '/\.foo\.com$/,/\.bar\.com$/'})
+    @override_settings(FOREST={'CORS_ALLOWED_ORIGIN_REGEXES': '/\.foo\.com$/,/\.bar\.com$/'})
     def test_set_cors_forest_settings(self):
         set_cors()
-        self.assertEqual(settings.CORS_ALLOWED_ORIGINS, ['http://localhost:4200', 'http://foo.com', 'https://bar.com'])
         self.assertEqual(settings.CORS_ALLOWED_ORIGIN_REGEXES, ['.*\\.forestadmin\\.com.*', '/\\.foo\\.com$/', '/\\.bar\\.com$/'])
--- django_forest/utils/cors.py
+++ django_forest/utils/cors.py
@@ -16,13 +16,9 @@ def set_cors():
         common_middleware_index = 0
     settings.MIDDLEWARE.insert(common_middleware_index, 'corsheaders.middleware.CorsMiddleware')
 
-    cors_allowed_origins = ['http://localhost:4200']
-    cors_allowed_origins.extend(get_list_setting('CORS_ALLOWED_ORIGINS'))
-
     cors_allowed_origins_regexes = [r'.*\.forestadmin\.com.*']
     cors_allowed_origins_regexes.extend(get_list_setting('CORS_ALLOWED_ORIGIN_REGEXES'))
 
-    settings.CORS_ALLOWED_ORIGINS = cors_allowed_origins
     settings.CORS_ALLOWED_ORIGIN_REGEXES = cors_allowed_origins_regexes
     settings.CORS_URLS_REGEX = r'^/forest(/.*)?$'  # restrict it to /forest
     settings.CORS_PREFLIGHT_MAX_AGE = 86400  # one day
