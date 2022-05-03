77c5eb8eae0eb75ecc9a3631992cf105becfa8c2
Valentin Monté <valentin.monte@yahoo.fr>
2022-05-03 12:18:02 +0200

    feat(cors): add the pna preflight headers to handle the last chrome versions implement a new middleware to handle pna headers

--- django_forest/tests/utils/test_cors.py
+++ django_forest/tests/utils/test_cors.py
@@ -12,7 +12,8 @@ class UtilsCorsTests(TestCase):
         set_cors()
         self.assertTrue('corsheaders' in settings.INSTALLED_APPS)
         self.assertTrue('corsheaders.middleware.CorsMiddleware' in settings.MIDDLEWARE)
-        self.assertEqual(settings.MIDDLEWARE.index('corsheaders.middleware.CorsMiddleware'), 0)
+        self.assertEqual(settings.MIDDLEWARE.index('django_forest.utils.cors.PnaMiddleware'), 0)
+        self.assertEqual(settings.MIDDLEWARE.index('corsheaders.middleware.CorsMiddleware'), 1)
         self.assertEqual(settings.CORS_ALLOWED_ORIGIN_REGEXES, ['.*\\.forestadmin\\.com.*'])
         self.assertEqual(settings.CORS_ALLOW_HEADERS, list(default_headers) + ['forest-context-url'])
         self.assertEqual(settings.CORS_ALLOW_CREDENTIALS, True)
@@ -25,7 +26,7 @@ class UtilsCorsTests(TestCase):
     @override_settings(MIDDLEWARE=['fake', 'django.middleware.X', 'corsheaders.middleware.CorsMiddleware'])
     def test_set_cors_middleware_already_present(self):
         set_cors()
-        self.assertEqual(settings.MIDDLEWARE, ['fake', 'django.middleware.X', 'corsheaders.middleware.CorsMiddleware'])
+        self.assertEqual(settings.MIDDLEWARE, ['fake', 'django.middleware.X', 'django_forest.utils.cors.PnaMiddleware', 'corsheaders.middleware.CorsMiddleware'])
     
     @override_settings(MIDDLEWARE=['fake', 'django.middleware.common.CommonMiddleware', 'corsheaders.middleware.CorsMiddleware'])
     def test_set_cors_with_common_middleware(self):
@@ -35,7 +36,7 @@ class UtilsCorsTests(TestCase):
     def test_set_cors_common_middleware(self):
         set_cors()
         self.assertTrue('corsheaders.middleware.CorsMiddleware' in settings.MIDDLEWARE)
-        self.assertEqual(settings.MIDDLEWARE, ['fakemiddleware', 'corsheaders.middleware.CorsMiddleware', 'django.middleware.common.CommonMiddleware', 'fakemiidleware2'])
+        self.assertEqual(settings.MIDDLEWARE, ['fakemiddleware', 'django_forest.utils.cors.PnaMiddleware', 'corsheaders.middleware.CorsMiddleware', 'django.middleware.common.CommonMiddleware', 'fakemiidleware2'])
 
     @override_settings(CORS_ALLOWED_ORIGIN_REGEXES=[r'/\.foo\.com$/',r'/\.bar\.com$/'])
     def test_extends_allowed_origin_regexes(self):
