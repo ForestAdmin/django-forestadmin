ab839aca0fc2daace785d06f187bdd36342065c3
Julien Barreau <julien.barreau@forestadmin.com>
2023-04-11 11:57:48 +0200

    fix(middlewares): users can force the forest middlwares orders

--- django_forest/tests/utils/test_middlewares.py
+++ django_forest/tests/utils/test_middlewares.py
@@ -5,10 +5,41 @@ from django_forest.utils.middlewares import set_middlewares
 
 
 class UtilsMiddlewaresTests(TestCase):
-
     def tearDown(self):
-        settings.MIDDLEWARE.remove('django_forest.middleware.PermissionMiddleware')
+        settings.MIDDLEWARE.remove("django_forest.middleware.PermissionMiddleware")
+        settings.MIDDLEWARE.remove("django_forest.middleware.IpWhitelistMiddleware")
 
     def test_set_middlewares(self):
         set_middlewares()
-        self.assertTrue('django_forest.middleware.PermissionMiddleware' in settings.MIDDLEWARE)
+        self.assertTrue(
+            "django_forest.middleware.PermissionMiddleware" in settings.MIDDLEWARE
+        )
+        self.assertTrue(
+            "django_forest.middleware.IpWhitelistMiddleware" in settings.MIDDLEWARE
+        )
+
+    def test_set_middlewares_already_present(self):
+        settings.MIDDLEWARE.insert(0, "django_forest.middleware.PermissionMiddleware")
+        settings.MIDDLEWARE.append("django_forest.middleware.IpWhitelistMiddleware")
+
+        set_middlewares()
+
+        permission_filtered_middlewares = list(
+            filter(
+                lambda x: x == "django_forest.middleware.PermissionMiddleware",
+                settings.MIDDLEWARE,
+            )
+        )
+        ip_whitelist_filtered_middlewares = list(
+            filter(
+                lambda x: x == "django_forest.middleware.IpWhitelistMiddleware",
+                settings.MIDDLEWARE,
+            )
+        )
+        # exists only once
+        self.assertEqual(len(permission_filtered_middlewares), 1)
+        self.assertEqual(len(ip_whitelist_filtered_middlewares), 1)
+
+        # order preserved
+        self.assertEqual(settings.MIDDLEWARE[0], "django_forest.middleware.PermissionMiddleware")
+        self.assertEqual(settings.MIDDLEWARE[-1], "django_forest.middleware.IpWhitelistMiddleware")
--- django_forest/utils/middlewares.py
+++ django_forest/utils/middlewares.py
@@ -2,5 +2,10 @@ from django.conf import settings
 
 
 def set_middlewares():
-    settings.MIDDLEWARE.insert(0, 'django_forest.middleware.PermissionMiddleware')
-    settings.MIDDLEWARE.insert(0, 'django_forest.middleware.IpWhitelistMiddleware')
+    middlewares = list(settings.MIDDLEWARE)
+
+    if 'django_forest.middleware.PermissionMiddleware' not in middlewares:
+        settings.MIDDLEWARE.insert(0, 'django_forest.middleware.PermissionMiddleware')
+
+    if 'django_forest.middleware.IpWhitelistMiddleware' not in middlewares:
+        settings.MIDDLEWARE.insert(0, 'django_forest.middleware.IpWhitelistMiddleware')
