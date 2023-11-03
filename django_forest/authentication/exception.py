4707f2d8ac6cd933c185a81da25d6b42b24a321f
Julien Barreau <julien.barreau@forestadmin.com>
2023-11-03 11:31:20 +0100

    fix(ahtentication_callback): return errors detail instead of generic error 500

--- django_forest/authentication/exception.py
+++ django_forest/authentication/exception.py
@@ -7,6 +7,12 @@ class BaseAuthenticationException(BaseForestException):
 class AuthenticationClientException(BaseAuthenticationException):
     STATUS = 401
 
+class AuthenticationOpenIdClientException(AuthenticationClientException):
+    def __init__(self, msg, error, error_description, state) -> None:
+        super().__init__(msg)
+        self.error = error
+        self.error_description = error_description
+        self.state = state
 
 class AuthenticationSettingsException(BaseAuthenticationException):
     STATUS = 500
--- django_forest/authentication/utils.py
+++ django_forest/authentication/utils.py
@@ -1,11 +1,21 @@
 from django.http import JsonResponse
-from django_forest.authentication.exception import BaseAuthenticationException
+from django_forest.authentication.exception import BaseAuthenticationException, AuthenticationOpenIdClientException
 
 
 def authentication_exception(f):
     def wrapper(*args, **kwargs):
         try:
             return f(*args, **kwargs)
+        except AuthenticationOpenIdClientException as error:
+            return JsonResponse(
+                {
+                    "error": error.error,
+                    "error_description": error.error_description,
+                    "state": error.state
+                },
+                status=error.STATUS
+            )
+
         except BaseAuthenticationException as error:
             return JsonResponse({'errors': [{'detail': str(error)}]}, status=error.STATUS)
     return wrapper
